9149eff73a47f63bdab297bb8359ce77423a6ad6
Panthomema <71283471+Panthomema@users.noreply.github.com>
2024-09-16 17:33:52 +0200

    Add aside mobile & pc static part

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import './styles/reset.css';
 import './styles/style.css';
 
+document.querySelector('#menu-btn').addEventListener('click', () => {
+  document.querySelector('aside').classList.toggle('aside-visible');
+});
 
-
+document.querySelector('#menu-btn').addEventListener('click', (event) => {
+  event.currentTarget.classList.toggle('rotated-180');
+});
