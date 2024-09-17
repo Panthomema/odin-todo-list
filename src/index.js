e34ee714d6f170ae9ad28e1e07848d6e8ae35bd5
Panthomema <71283471+Panthomema@users.noreply.github.com>
2024-09-17 10:45:14 +0200

    Attempt layout fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import './styles/reset.css';
 import './styles/style.css';
+import inbox from './tabs/inbox';
 
 document.querySelector('#menu-btn').addEventListener('click', () => {
-  document.querySelector('aside').classList.toggle('aside-visible');
+  document.querySelector('nav').classList.toggle('nav-visible');
 });
 
 document.querySelector('#menu-btn').addEventListener('click', (event) => {
   event.currentTarget.classList.toggle('rotated-180');
+});
+
+document.querySelector('#inbox-btn').addEventListener('click', () => {
+  document.querySelector('main').appendChild(inbox);
 });
---
+++ src/tabs/inbox.js
@@ -0,0 +1,7 @@
+const inbox = document.createElement('div');
+inbox.classList.add('content');
+
+inbox.innerHTML = '<h2>Inbox</h2>'
+
+export default inbox;
+
