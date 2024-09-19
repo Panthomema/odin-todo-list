31108aad6b0251941918edc6c93c948dc9ffdfed
Panthomema <71283471+Panthomema@users.noreply.github.com>
2024-09-19 11:56:26 +0200

    Add Overlay to medium-sized screens

--- src/index.js
+++ src/index.js
@@ -10,6 +10,22 @@ document.querySelector('#menu-btn').addEventListener('click', (event) => {
   event.currentTarget.classList.toggle('rotated-180');
 });
 
+document.querySelector('#menu-btn').addEventListener('click', () => {
+  const overlay = document.querySelector('.overlay');
+
+  if (overlay.classList.contains('visible')) {
+    overlay.addEventListener('transitionend', () => {
+      overlay.classList.remove('displayed');
+    }, { once: true });
+    overlay.classList.remove('visible');
+  } else {
+    overlay.classList.add('displayed');
+    setTimeout(() => {
+      overlay.classList.add('visible');
+    }, 1);
+  }
+});
+
 document.querySelector('#inbox-btn').addEventListener('click', () => {
   document.querySelector('main').appendChild(inbox);
 });
